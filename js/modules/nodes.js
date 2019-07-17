19f92f452c8996de7d02af114b66d7adca46f243
JoeMancera <joeharrymancera@gmail.com>
2019-07-17 14:37:52 -0500

    condiciona para mostrar la accion de los nodos

---
+++ js/modules/nodes.js
@@ -0,0 +1,44 @@
+async function infoNode(idNode){
+    document.querySelector('#load').style = "display: block;"
+    let node = await _getNode(idNode);
+    let infoNode =  node.results.data;
+    document.querySelector('#node-name').innerHTML = infoNode.properties.name
+    let children = await _getChildren(idNode);
+    emptyTableNode();
+    const childs = children.data.length;
+    
+    for (let i = 0; i < childs; i++) {
+        let tableRef = document.getElementById('node-children').getElementsByTagName('tbody')[0];
+        let newRow   = tableRef.insertRow();
+        let newCell0  = newRow.insertCell(0);
+        let nameNode  = document.createTextNode(children.data[i].name);
+        
+        switch (children.data[i].type) {
+            case 0:
+                newCell0.setAttribute('onclick', `infoNode(${children.data[i].id})` );
+                break;
+            case 1:
+                newCell0.setAttribute('onclick', `infoNode(${children.data[i].original_id})` );
+                break;
+            case 144:
+                newCell0.setAttribute('onclick', `infoNode(${children.data[i].original_id})` );
+                break;
+            default:
+            newCell0.setAttribute('onclick', `infoNode(${children.data[i].id})` );
+            break;
+        }
+        
+        newCell0.appendChild(nameNode);
+        newCell0.style.cursor='pointer';
+        newCell0.classList.add("item-node-list");
+        let newCell1  = newRow.insertCell(1);
+        let typeNode  = document.createTextNode(children.data[i].type_name);
+        newCell1.appendChild(typeNode);
+        let newCell2  = newRow.insertCell(2);
+        let contNode  = document.createTextNode(children.data[i].size_formatted);
+        newCell2.appendChild(contNode);
+        let newCell3  = newRow.insertCell(3);
+    }
+    document.querySelector('#load').style = "display: none;"
+    return true;
+}
